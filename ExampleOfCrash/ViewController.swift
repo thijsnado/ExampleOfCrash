47998c035fca3173d4a8083a1a60fc1074ba4808
Thijs de Vries <moger777@gmail.com>
2016-11-20 20:17:40 -0500

    Initial Commit

---
+++ ExampleOfCrash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExampleOfCrash
+//
+//  Created by Thijs de Vries on 11/20/16.
+//  Copyright © 2016 Thijs de Vries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
